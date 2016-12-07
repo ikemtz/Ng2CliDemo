8e3d301807175619854bb09a1f86530e716fe071
Isaac Martinez <pcalchi@aim.com>
2016-12-07 11:29:40 -0500

    Extending Blog Service

--- src/app/blogs.service.ts
+++ src/app/blogs.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import { IBlog } from './iBlog';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class BlogsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  private url: string = 'https://derivewebnode.azurewebsites.net/api/blogs';  // URL to web api
 
+  public GetBlogsResponse(): Promise<IBlog[]> {
+    return this.http.get(this.url)
+      .toPromise()
+      .then(response => {
+        let r = response.json() as IBlog[];
+        return r;
+      })
+      .catch(this.handleError);
+  }
+
+  public handleError(error: any): Promise<any> {
+
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
